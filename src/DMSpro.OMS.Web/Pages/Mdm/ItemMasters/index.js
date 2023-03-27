845402de52f412be3cc5881818365098eba9b738
Quang Nguyen <ndq.6895a@gmail.com>
2023-03-27 10:10:14 +0700

    change default itemtype selected value

--- src/DMSpro.OMS.Web/Pages/Mdm/ItemMasters/index.js
+++ src/DMSpro.OMS.Web/Pages/Mdm/ItemMasters/index.js
@@ -580,7 +580,7 @@
             e.data.isInventoriable = true;
             e.data.isPurchasable = true;
             e.data.isSaleable = true;
-            e.data.itemType = 'I';
+            e.data.itemTypeId = '5b5c2587-e1e8-2083-1ba9-3a09f04363e2';
         },
         onRowUpdating: function (e) {
             e.newData = Object.assign({}, e.oldData, e.newData);
