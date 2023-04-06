3075a5a663c9af6db0e1e8c2bbcadf44a53c44e2
Quang Nguyen <ndq.6895a@gmail.com>
2023-04-06 16:30:54 +0700

    update disable color, optimize layout

--- src/DMSpro.OMS.Web/Pages/Mdm/ItemAttributes/index.js
+++ src/DMSpro.OMS.Web/Pages/Mdm/ItemAttributes/index.js
@@ -1,20 +1,7 @@
-﻿let sample = () => {
-    createFlat(input, ajaxParams)
-    createHierarchy(input, ajaxParams)
-    delete (ajaxParams)
-    generateExcelTemplates(ajaxParams)
-    get(id, ajaxParams)
-    getListDevextremes(inputDev, ajaxParams)
-    insertFromExcel(file, ajaxParams)
-    reset(ajaxParams)
-    update(id, input, ajaxParams)
-    updateFromExcel(file, ajaxParams)
-}
-$(function () {
-    let gridInfo = {}, sendMode = 0
+﻿$(function () {
+    let gridInfo = {}, sendMode
     let itemAttrService = window.dMSpro.oMS.mdmService.controllers.itemAttributes.itemAttribute;
     let l = abp.localization.getResource("OMS");
-
     let dialog = ({ header, body }, callBackIfTrue, callBackIfFalse) => {
         DevExpress.ui.dialog.confirm(`<i>${body}</i>`, header)
             .done((e) => {
@@ -26,7 +13,6 @@ $(function () {
                 }
             })
     }
-
     let customStore = new DevExpress.data.CustomStore({
         key: "id",
         loadMode: 'raw',
@@ -39,7 +25,14 @@ $(function () {
                     args[i] = JSON.stringify(loadOptions[i]);
                 }
             });
-            itemAttrService.getListDevextremes(args)
+            itemAttrService.getListDevextremes(
+                JSON.stringify({
+                    sort: {
+                        selector: "attrNo",
+                        desc: false
+                    }
+                }),
+            )
                 .done(result => {
                     gridInfo = {
                         ...gridInfo,
@@ -47,7 +40,7 @@ $(function () {
                         lastLevel: result.data.filter(e => e.active).length,
                         canReset: result.summary[0].canReset === "False" ? false : true
                     }
-                    deferred.resolve(result.data, {
+                    deferred.resolve(result.data.sort((a, b) => a.attrNo - b.attrNo), {
                         totalCount: result.totalCount,
                         summary: result.summary,
                         groupCount: result.groupCount
@@ -77,7 +70,6 @@ $(function () {
             return itemAttrService.update(key, { attrName });
         },
         remove(key) {
-            // Delete , return full DataSource 
             return itemAttrService.delete({});
         }
     });
@@ -87,9 +79,7 @@ $(function () {
                 store: customStore,
                 filter: ['active', '=', true]
             },
-            // remoteOperations: true,
             showRowLines: true,
-            showColumnLines: false,
             showBorders: true,
             cacheEnabled: true,
             allowColumnReordering: true,
@@ -97,40 +87,10 @@ $(function () {
             allowColumnResizing: true,
             columnResizingMode: 'widget',
             columnAutoWidth: true,
-            filterRow: {
-                visible: true
-            },
-            height: '80vh',
             columnMinWidth: 50,
             columnFixing: {
                 enabled: true,
             },
-            // export: {
-            //     enabled: true,
-            // },
-            // onExporting(e) {
-            //     const workbook = new ExcelJS.Workbook();
-            //     const worksheet = workbook.addWorksheet('Data');
-
-            //     DevExpress.excelExporter.exportDataGrid({
-            //         component: e.component,
-            //         worksheet,
-            //         autoFilterEnabled: true,
-            //     }).then(() => {
-            //         workbook.xlsx.writeBuffer().then((buffer) => {
-            //             saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'ItemAttributes.xlsx');
-            //         });
-            //     });
-            //     e.cancel = true;
-            // },
-            headerFilter: {
-                visible: true,
-            },
-            // stateStoring: {
-            //     enabled: true,
-            //     type: 'localStorage',
-            //     storageKey: 'gridProdAttribute',
-            // },
             editing: {
                 mode: "row",
                 allowAdding: abp.auth.isGranted('MdmService.ItemAttributes.Create') && gridInfo.length < 20,
@@ -148,12 +108,8 @@ $(function () {
                 e.newData = Object.assign({}, e.oldData, e.newData);
             },
             onRowInserting: (e) => {
-                console.log('1');
                 e.data.active = true
                 e.data.sendMode = sendMode
-            },
-            onInitialized: () => {
-                console.log(gridInfo);
             },
             toolbar: {
                 items: [
@@ -164,7 +120,7 @@ $(function () {
                         options: {
                             icon: "refresh",
                             disabled: !gridInfo.canReset || !abp.auth.isGranted('MdmService.ItemAttributes.Delete'),
-                            onClick(e) {
+                            onClick() {
                                 dialog(
                                     {
                                         header: l('EntityFieldName:MDMService:ItemAttribute:Reset:ConfirmationHeader'),
@@ -176,7 +132,6 @@ $(function () {
                                         })
                                     },
                                 )
-                                console.log("AAAA");
                             }
                         },
                     },
@@ -186,6 +141,7 @@ $(function () {
                 {
                     type: 'buttons',
                     alignment: 'left',
+                    width: 70,
                     buttons: ["edit", {
                         name: 'delete',
                         visible: (e) => {
@@ -196,13 +152,6 @@ $(function () {
                     caption: l('Actions'),
                     fixedPosition: 'left',
 
-                },
-                {
-                    dataField: 'attrNo',
-                    width: 100,
-                    sortIndex: 0, sortOrder: "asc",
-                    caption: l("EntityFieldName:MDMService:ItemAttribute:AttrNo"),
-                    allowEditing: false
                 },
                 {
                     dataField: 'attrName',
@@ -220,43 +169,21 @@ $(function () {
                     caption: l("EntityFieldName:MDMService:ItemAttribute:HierarchyLevel"),
                     alignment: 'center',
                     editCellTemplate: (cellElement, cellInfo) => {
-                        console.log(cellInfo);
-                        return $('<div>').dxCheckBox({
-                            value: false,
-                            onValueChanged(e) {
-                                console.log('2');
-                                if (e.value) {
-                                    sendMode = 1
-                                } else {
-                                    sendMode = 0
-                                }
-                            },
-                        })
-                    }
+                        console.log(cellInfo.row.isNewRow);
+                        if (cellInfo.row.isNewRow)
+                            return $('<div>').dxCheckBox({
+                                value: false,
+                                onValueChanged(e) {
+                                    if (e.value) {
+                                        sendMode = 1
+                                    } else {
+                                        sendMode = 0
+                                    }
+                                },
+                            })
+                        return cellElement.css('background-color', "#5c95c5")
+                    },
                 },
-                // {
-                //     dataField: 'active',
-                //     width: 110,
-                //     caption: l("EntityFieldName:MDMService:ItemAttribute:Active"),
-                //     alignment: 'center',
-                //     dataType: 'boolean',
-                //     cellTemplate(container, options) {
-                //         $('<div>')
-                //             .append($(options.value ? '<i class="fa fa-check" style="color:#34b233"></i>' : '<i class= "fa fa-times" style="color:red"></i>'))
-                //             .appendTo(container);
-                //     }
-                // },
-                // {
-                //     dataField: 'isSellingCategory',
-                //     caption: l("EntityFieldName:MDMService:ItemAttribute:IsSellingCategory"),
-                //     alignment: 'center',
-                //     dataType: 'boolean',
-                //     cellTemplate(container, options) {
-                //         $('<div>')
-                //             .append($(options.value ? '<i class="fa fa-check" style="color:#34b233"></i>' : '<i class= "fa fa-times" style="color:red"></i>'))
-                //             .appendTo(container);
-                //     }
-                // }
             ]
         }).dxDataGrid('instance');
     })
