43b83e4f07f75c46626cb2034012d1ed8d098050
Wharang <>
2023-07-19 23:19:57 +0700

    SAPCustomer * Add Attribute Tab

--- src/DMSpro.OMS.Web/Pages/MdmSAP/CustomerProfile/js/tabs/attribute.js
+++ src/DMSpro.OMS.Web/Pages/MdmSAP/CustomerProfile/js/tabs/attribute.js
@@ -4,26 +4,134 @@ let createAttribute = () => {
         icon: null,
         callback: () => {
             let container = $('<div/>')
-            let form = $('<div/>').dxForm({
-                formData: {
-                    ...gridInfo.data,
-                },
-                labelMode: "outside",
-                colCount: 2,
-                readOnly,
-                items: [
+            let datagrid = $('<div/>').dxDataGrid({
+                dataSource: [
                     {
-                        itemType: "group",
-                        colCount: 2,
-                        colSpan: 2,
-                        items: getAttrField('flat')
+                        ...gridInfo.data,
+                    }
+                ],
+                allowColumnResizing: true,
+                columnResizingMode: 'widget',
+                columnAutoWidth: true,
+                remoteOperations: true,
+                showRowLines: true,
+                showBorders: true,
+                cacheEnabled: true,
+                rowAlternationEnabled: true,
+                repaintChangesOnly: true,
+                filterRow: {
+                    visible: true
+                },
+                groupPanel: {
+                    visible: true,
+                },
+                searchPanel: {
+                    visible: true
+                },
+                headerFilter: {
+                    visible: true,
+                },
+
+                paging: {
+                    enabled: true,
+                    pageSize
+                },
+                pager: {
+                    visible: true,
+                    showPageSizeSelector: true,
+                    allowedPageSizes,
+                    showInfo: true,
+                    showNavigationButtons: true
+                },
+                columnMinWidth: 50,
+                columnChooser: {
+                    enabled: true,
+                    mode: "select"
+                },
+                export: {
+                    enabled: true,
+                },
+                onExporting: function (e) {
+                    const workbook = new ExcelJS.Workbook();
+                    const worksheet = workbook.addWorksheet('Data');
+                    DevExpress.excelExporter.exportDataGrid({
+                        component: e.component,
+                        worksheet,
+                        autoFilterEnabled: true,
+                    }).then(() => {
+                        workbook.xlsx.writeBuffer().then((buffer) => {
+                            saveAs(new Blob([buffer], { type: 'application/octet-stream' }), `ItemInventory.xlsx`);
+                        });
+                    });
+                    e.cancel = true;
+                },
+                toolbar: {
+                    items: [
+                        "groupPanel",
+                        'addRowButton',
+                        'columnChooserButton',
+                        "exportButton",
+                        "searchPanel"
+                    ]
+                },
+                columns: [
+                    {
+                        caption: "Ship To Code", // Localize
+                        dataField: "ShipToCode",
+                        dataType: 'string',
                     },
                     {
-                        itemType: 'empty',
-                        colSpan: 2
-                    }
+                        caption: "Attribute 0", // Localize
+                        dataField: "AttrName0",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 1", // Localize
+                        dataField: "AttrName1",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 2", // Localize
+                        dataField: "AttrName2",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 3", // Localize
+                        dataField: "AttrName3",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 4", // Localize
+                        dataField: "AttrName4",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 5", // Localize
+                        dataField: "AttrName5",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 6", // Localize
+                        dataField: "AttrName6",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 7", // Localize
+                        dataField: "AttrName7",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 8", // Localize
+                        dataField: "AttrName8",
+                        dataType: 'string',
+                    },
+                    {
+                        caption: "Attribute 9", // Localize
+                        dataField: "AttrName9",
+                        dataType: 'string',
+                    },
                 ]
-            }).appendTo(container).dxForm('instance')
+            }).appendTo(container)
             return container
         }
     }
